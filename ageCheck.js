cb688fe1569b321fb10f28f9cd0c11f01b250a88
Pabsyyy <christiangalinato1218@gmail.com>
2024-10-09 18:00:32 +0800

    inital commit

---
+++ ageCheck.js
@@ -0,0 +1,10 @@
+function ageCheck() {
+    let age = prompt("Enter Your Age: ");
+    if (age <=12) {
+        alert("You are a Child.");
+    } else if (age <=19) {
+        alert("You are a Teenager. ");
+    } else {
+        alert("You are an Adult. ");
+    }
+} 
